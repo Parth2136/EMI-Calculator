e22799c98cfec254dbbb5b70fc68e9f402dca017
Parth2136 <parthjasani2136@gmail.com>
2022-05-10 15:19:25 +0530

    commit

--- app/src/main/java/com/example/emicalculator/MainActivity.java
+++ app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -11,30 +11,57 @@ import android.view.View;
 import com.example.emicalculator.R;
 
 public class MainActivity extends AppCompatActivity  {
-    Button calculate;
-    TextView result;
-    EditText pe, ne, re;
+
+    EditText amount_edt , int_edt , month_edt;
+    Button emi;
+    TextView emi_txt , total_txt , extra_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calculate =  findViewById(R.id.button);
-        result =  findViewById(R.id.textView);
-        pe = findViewById(R.id.editText);
-        ne = findViewById(R.id.editText2);
-        re = findViewById(R.id.editText3);
-        calculate.setOnClickListener(new View.OnClickListener() {
+        amount_edt =findViewById(R.id.amount_edt);
+        int_edt =findViewById(R.id.int_edt);
+        month_edt =findViewById(R.id.month_edt);
+        emi =findViewById(R.id.emi);
+        emi_txt =findViewById(R.id.emi_txt);
+        total_txt =findViewById(R.id.total_txt);
+        extra_txt =findViewById(R.id.extra_txt);
+
+        emi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Double p = Double.parseDouble(pe.getText().toString());
-                Double n = Double.parseDouble(ne.getText().toString());
-                Double r = Double.parseDouble(re.getText().toString());
-                Double rate= (r/1200);
-                Double res = (p * rate * Math.pow(1 + rate, n) / Math.pow(1 + rate, n) - 1);
-                result.setText("EMI = "+String.valueOf(res));
+
+                if(amount_edt.getText().toString().isEmpty()) {
+                    amount_edt.setError("Enter Amount!!");
+                } else if (int_edt.getText().toString().isEmpty()) {
+                    int_edt.setError("Enter Intrest!!");
+                } else if (month_edt.getText().toString().isEmpty()) {
+                    month_edt.setError("Enter Month!!");
+                } else {
+
+                    double amt = Double.parseDouble(amount_edt.getText().toString());
+                    double rate = Double.parseDouble(int_edt.getText().toString());
+                    double month = Double.parseDouble(month_edt.getText().toString());
+
+                    double r = rate / (12 * 100);
+
+                    double cal = 1;
+
+                    for(int i=0; i<month ; i++) {
+                        cal *= (1 + r);
+                    }
+
+                    double ans =amt * r * (cal / (cal-1));
+
+                    emi_txt.setText("EMI = "+ans);
+                    total_txt.setText("Total = "+(ans*month));
+                    extra_txt.setText("Intrest Amt = "+(ans*month-amt));
+
+                }
             }
         });
+
     }
 }
